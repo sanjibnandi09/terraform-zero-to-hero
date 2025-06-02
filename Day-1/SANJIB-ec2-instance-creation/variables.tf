5e134e31efa40aa8786668cfb21f328af34c2a3a
sanjibnandi09 <64964065+sanjibnandi09@users.noreply.github.com>
2025-06-02 15:47:36 +0000

    ec2 instance

---
+++ Day-1/SANJIB-ec2-instance-creation/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+  description = "ami value in use"
+}
+variable "instance_type_value" {
+  description = "value for instance type"
+}
+variable "subnet_id_value" {
+  description = "value for the subnet_id"
+}
