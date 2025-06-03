c18839e6cf451d7b7eae8f2f1ddd663928db80fe
sanjibnandi09 <64964065+sanjibnandi09@users.noreply.github.com>
2025-06-03 14:17:47 +0000

    backend

--- Day-1/SANJIB-ec2-instance-creation/backend.tf
+++ Day-1/SANJIB-ec2-instance-creation/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-aws-bucket-nandis"
     key = "sanjib/terraform.tfstate"
+    use_lockfile = true
     region = "ap-south-1"
   }
 }
--- Day-1/SANJIB-ec2-instance-creation/main.tf
+++ Day-1/SANJIB-ec2-instance-creation/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 
 module "ec2_instance" {
   source = "./module_ec2"
-  ami_value = ""
-  instance_type_value = ""
-  subnet_id_value = ""
+  ami_value = "ami-0f535a71b34f2d44a"
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-09e73c8e079c437e3"
 
 }
 /*
